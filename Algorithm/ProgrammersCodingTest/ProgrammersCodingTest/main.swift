0cd6c087d7733b517a28be728f9dfe605534e0f6
InhyeKang <kanginhae0826@gmail.com>
2023-05-06 23:39:27 +0900

    개인정보 수집 유효기간(150370)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -8,11 +8,38 @@
 import Foundation
 
 // 중복된 문자 제거(120888)
+//func solution(_ my_string: String) -> String {
+//    var result = ""
+//    for str in my_string where !result.contains(str) {
+//        result.append(str)
+//    }
+//    return result
+//}
 
-func solution(_ my_string: String) -> String {
-    var result = ""
-    for str in my_string where !result.contains(str) {
-        result.append(str)
+
+
+// 개인정보 수집 유효기간(150370)
+func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
+    var answer = [Int]()
+
+    let today = getIntValueFromDateString(date: today)
+    let expiration = terms.reduce(into: [String: Int]()) { dict, str in
+        let info = str.components(separatedBy: " ")
+        dict[info[0]] = Int(info[1])!
     }
-    return result
+
+    privacies.enumerated().forEach { (offset, privacy) in
+        let temp = privacy.components(separatedBy: " ")
+        let beginDate = getIntValueFromDateString(date: temp[0])
+        let isExpired =  beginDate + expiration[temp[1]]! * 28 <= today
+        if isExpired { answer.append(offset + 1) }
+    }
+
+    return answer
+}
+
+
+func getIntValueFromDateString(date: String) -> Int {
+    let info = date.components(separatedBy: ".").map { Int($0)! }
+    return info[0] * 12 * 28 + info[1] * 28 + info[2]
 }
