6a95ee18ffd8b17798da3e03273062c60048119d
InhyeKang <kanginhae0826@gmail.com>
2023-05-07 23:27:18 +0900

    신고 결과 받기(92334)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -19,27 +19,46 @@ import Foundation
 
 
 // 개인정보 수집 유효기간(150370)
-func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
-    var answer = [Int]()
+//func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
+//    var answer = [Int]()
+//
+//    let today = getIntValueFromDateString(date: today)
+//    let expiration = terms.reduce(into: [String: Int]()) { dict, str in
+//        let info = str.components(separatedBy: " ")
+//        dict[info[0]] = Int(info[1])!
+//    }
+//
+//    privacies.enumerated().forEach { (offset, privacy) in
+//        let temp = privacy.components(separatedBy: " ")
+//        let beginDate = getIntValueFromDateString(date: temp[0])
+//        let isExpired =  beginDate + expiration[temp[1]]! * 28 <= today
+//        if isExpired { answer.append(offset + 1) }
+//    }
+//
+//    return answer
+//}
+//
+//
+//func getIntValueFromDateString(date: String) -> Int {
+//    let info = date.components(separatedBy: ".").map { Int($0)! }
+//    return info[0] * 12 * 28 + info[1] * 28 + info[2]
+//}
 
-    let today = getIntValueFromDateString(date: today)
-    let expiration = terms.reduce(into: [String: Int]()) { dict, str in
-        let info = str.components(separatedBy: " ")
-        dict[info[0]] = Int(info[1])!
+
+//신고 결과 받기(92334)
+func solution(_ id_list: [String], _ report: [String], _ k: Int) -> [Int] {
+    var reported: [String: Int] = [:]
+    var user: [String: [String]] = [:]
+
+    for rep in Set(report) { //Set으로 중복 제거하면 시간 초과 안 뜸
+        let splited = rep.split(separator: " ").map { String($0) }
+        user[splited[0]] = (user[splited[0]] ?? []) + [splited[1]] //prosecutor
+        reported[splited[1]] = (reported[splited[1]] ?? 0) + 1 //suspect
     }
 
-    privacies.enumerated().forEach { (offset, privacy) in
-        let temp = privacy.components(separatedBy: " ")
-        let beginDate = getIntValueFromDateString(date: temp[0])
-        let isExpired =  beginDate + expiration[temp[1]]! * 28 <= today
-        if isExpired { answer.append(offset + 1) }
+    return id_list.map { id in
+        return (user[id] ?? []).reduce(0) {
+            $0 + ((reported[$1] ?? 0) >= k ? 1 : 0)
+        }
     }
-
-    return answer
-}
-
-
-func getIntValueFromDateString(date: String) -> Int {
-    let info = date.components(separatedBy: ".").map { Int($0)! }
-    return info[0] * 12 * 28 + info[1] * 28 + info[2]
 }
