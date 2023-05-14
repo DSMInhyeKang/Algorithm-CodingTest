637420b06d11a3425fe5ad64f1ec1d448bffd361
InhyeKang <kanginhae0826@gmail.com>
2023-05-14 22:51:15 +0900

    공원 산책(172928)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -46,19 +46,65 @@ import Foundation
 
 
 //신고 결과 받기(92334)
-func solution(_ id_list: [String], _ report: [String], _ k: Int) -> [Int] {
-    var reported: [String: Int] = [:]
-    var user: [String: [String]] = [:]
+//func solution(_ id_list: [String], _ report: [String], _ k: Int) -> [Int] {
+//    var reported: [String: Int] = [:]
+//    var user: [String: [String]] = [:]
+//
+//    for rep in Set(report) { //Set으로 중복 제거하면 시간 초과 안 뜸
+//        let splited = rep.split(separator: " ").map { String($0) }
+//        user[splited[0]] = (user[splited[0]] ?? []) + [splited[1]] //prosecutor
+//        reported[splited[1]] = (reported[splited[1]] ?? 0) + 1 //suspect
+//    }
+//
+//    return id_list.map { id in
+//        return (user[id] ?? []).reduce(0) {
+//            $0 + ((reported[$1] ?? 0) >= k ? 1 : 0)
+//        }
+//    }
+//}
 
-    for rep in Set(report) { //Set으로 중복 제거하면 시간 초과 안 뜸
-        let splited = rep.split(separator: " ").map { String($0) }
-        user[splited[0]] = (user[splited[0]] ?? []) + [splited[1]] //prosecutor
-        reported[splited[1]] = (reported[splited[1]] ?? 0) + 1 //suspect
-    }
 
-    return id_list.map { id in
-        return (user[id] ?? []).reduce(0) {
-            $0 + ((reported[$1] ?? 0) >= k ? 1 : 0)
+// 공원 산책(172928)
+func solution(_ park: [String], _ routes: [String]) -> [Int] {
+    var (park, x, y) = (park.map { $0.map { String($0) } }, 0, 0)
+    
+    for i in 0..<park.count {
+        for j in 0..<park[0].count where park[i][j] == "S" {
+             (x, y) = (i, j)
         }
     }
+    
+    func isIn(_ x: Int, _ y: Int) -> Bool {
+        x >= 0 && y >= 0 && x < park.count && y < park[0].count
+    }
+
+    for d in routes {
+        let d = d.split { $0 == " "}.map { String ($0) }
+        let (dir, wei) = (d[0], d[1])
+        var (ixiy, skip) = ((0, 0), false)
+        
+        switch dir {
+            case "N":
+                ixiy = (-1, 0)
+            case "S":
+                ixiy = (1, 0)
+            case "W":
+                ixiy = (0, -1)
+            default:
+                ixiy = (0, 1)
+        }
+        
+        var (X, Y) = (x, y)
+
+        for _ in 0..<Int(wei)! where !skip {
+            (X, Y) = (X + ixiy.0, Y + ixiy.1)
+            if !isIn(X, Y) { skip = true; break }
+            if park[X][Y] == "X" { skip = true }
+        }
+        
+        if skip { continue }
+        (x, y) = (X, Y)
+    }
+    
+    return [x, y]
 }
