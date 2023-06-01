fbb4d307a267096d86dde9a668714a408392bdb6
InhyeKang <kanginhae0826@gmail.com>
2023-06-01 23:46:30 +0900

    신규 아이디 추천(72410)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -111,6 +111,50 @@ import Foundation
 
 
 // 수박수박수박수박수박수?(12922)
-func solution(_ n:Int) -> String {
-    return (0..<n).map{($0%2==0 ? "수":"박")}.reduce("", +)
+//func solution(_ n:Int) -> String {
+//    return (0..<n).map{($0%2==0 ? "수":"박")}.reduce("", +)
+//}
+
+
+
+// 신규 아이디 추천(72410)
+func solution(_ new_id:String) -> String {
+    var new = ""
+
+    new_id.forEach {
+        if $0.isLowercase || Int(String($0)) != nil || $0 == "." || $0 == "_" || $0 == "-" {
+            if !(new.isEmpty && $0 == ".") && !(new.last == "." && $0 == ".") {
+                new.append($0)
+            }
+        } else {
+            if $0.isUppercase {
+                new.append($0.lowercased())
+            }
+        }
+    }
+
+    if new.last == "." {
+        new.removeLast()
+    }
+
+    if new.count > 15 {
+        new.removeSubrange(new.index(after: new.index(new.startIndex, offsetBy: 14)) ..< new.endIndex)
+    } else if new.count <= 2{
+        if new.count <= 2 {
+            if new.isEmpty {
+                new = "a"
+            }
+
+            while new.count < 3 {
+                new.append(new.last!)
+            }
+        }
+    }
+
+
+    if new.last == "." {
+        new.removeLast()
+    }
+
+    return new
 }
