9e4793c7738f2d2dadf32636c807a1749fd79f82
InhyeKang <kanginhae0826@gmail.com>
2023-06-10 23:30:48 +0900

    달리기 경주(178871)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -118,43 +118,67 @@ import Foundation
 
 
 // 신규 아이디 추천(72410)
-func solution(_ new_id:String) -> String {
-    var new = ""
+//func solution(_ new_id:String) -> String {
+//    var new = ""
+//
+//    new_id.forEach {
+//        if $0.isLowercase || Int(String($0)) != nil || $0 == "." || $0 == "_" || $0 == "-" {
+//            if !(new.isEmpty && $0 == ".") && !(new.last == "." && $0 == ".") {
+//                new.append($0)
+//            }
+//        } else {
+//            if $0.isUppercase {
+//                new.append($0.lowercased())
+//            }
+//        }
+//    }
+//
+//    if new.last == "." {
+//        new.removeLast()
+//    }
+//
+//    if new.count > 15 {
+//        new.removeSubrange(new.index(after: new.index(new.startIndex, offsetBy: 14)) ..< new.endIndex)
+//    } else if new.count <= 2{
+//        if new.count <= 2 {
+//            if new.isEmpty {
+//                new = "a"
+//            }
+//
+//            while new.count < 3 {
+//                new.append(new.last!)
+//            }
+//        }
+//    }
+//
+//
+//    if new.last == "." {
+//        new.removeLast()
+//    }
+//
+//    return new
+//}
 
-    new_id.forEach {
-        if $0.isLowercase || Int(String($0)) != nil || $0 == "." || $0 == "_" || $0 == "-" {
-            if !(new.isEmpty && $0 == ".") && !(new.last == "." && $0 == ".") {
-                new.append($0)
-            }
-        } else {
-            if $0.isUppercase {
-                new.append($0.lowercased())
-            }
-        }
+
+
+// 달리기 경주(178871)
+func solution(_ players:[String], _ callings:[String]) -> [String] {
+    var players = players
+    var callings = callings
+    var dict = [String:Int]()
+    
+    for i in 0..<players.count {
+        dict[players[i]] = i
     }
-
-    if new.last == "." {
-        new.removeLast()
+    
+    for j in 0..<callings.count {
+        var rank = dict[callings[j]]!
+        let name  = players[rank - 1]
+        players[rank - 1] = callings[j]
+        players[rank] = name
+        dict[callings[j]]! -= 1
+        dict[players[rank]]! += 1
     }
-
-    if new.count > 15 {
-        new.removeSubrange(new.index(after: new.index(new.startIndex, offsetBy: 14)) ..< new.endIndex)
-    } else if new.count <= 2{
-        if new.count <= 2 {
-            if new.isEmpty {
-                new = "a"
-            }
-
-            while new.count < 3 {
-                new.append(new.last!)
-            }
-        }
-    }
-
-
-    if new.last == "." {
-        new.removeLast()
-    }
-
-    return new
+    
+    return players
 }
