e06a3bdddaafa79fcb769eecbd24a39bae36c42f
InhyeKang <kanginhae0826@gmail.com>
2023-07-30 22:25:36 +0900

    문자열 나누기(140108)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -416,8 +416,34 @@ import Foundation
 
 
 // 과일 장수(135808)
-func solution(_ k: Int, _ m: Int, _ score: [Int]) -> Int {
-    let s = score.sorted(by: >)
-       
-    return stride(from: m-1, to: score.count, by: m).reduce(0) { $0 + s[$1] * m }
+//func solution(_ k: Int, _ m: Int, _ score: [Int]) -> Int {
+//    let s = score.sorted(by: >)
+//
+//    return stride(from: m-1, to: score.count, by: m).reduce(0) { $0 + s[$1] * m }
+//}
+
+
+
+// 문자열 나누기(140108)
+func solution(_ s: String) -> Int {
+    var answer = 0
+    var x: Character? = nil
+    var xCount = 0
+
+    for i in s {
+        if x == nil {
+            x = i
+            xCount = 1
+            answer += 1
+            continue
+        }
+
+        xCount += x == i ? 1 : -1
+
+        if xCount == 0 {
+            x = nil
+        }
+    }
+
+    return answer
 }
