3a8fe6a2674cac747c9c78469a313ee63990e302
InhyeKang <kanginhae0826@gmail.com>
2023-08-08 23:47:04 +0900

    땅따먹기(12913)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -652,23 +652,41 @@ import Foundation
 
 
 // 가장 큰 정사각형 찾기(12905)
-func solution(_ board:[[Int]]) -> Int {
-    var answer:Int = 0
-    var dp = board
+//func solution(_ board:[[Int]]) -> Int {
+//    var answer:Int = 0
+//    var dp = board
+//
+//    for i in 0..<board.count {
+//        for j in 0..<board[0].count {
+//            answer = max(answer, board[i][j])
+//            if i - 1 < 0 || j - 1 < 0 {
+//                continue
+//            }
+//
+//            if board[i][j] == 1 && board[i-1][j] == 1 && board[i][j-1] == 1 {
+//                dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
+//                answer = max(answer, dp[i][j])
+//            }
+//        }
+//    }
+//
+//    return answer * answer
+//}
+
+
+
+// 땅따먹기(12913)
+func solution(_ land: [[Int]]) -> Int {
+    var dp = land
     
-    for i in 0..<board.count {
-        for j in 0..<board[0].count {
-            answer = max(answer, board[i][j])
-            if i - 1 < 0 || j - 1 < 0 {
-                continue
-            }
-            
-            if board[i][j] == 1 && board[i-1][j] == 1 && board[i][j-1] == 1 {
-                dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
-                answer = max(answer, dp[i][j])
+    for i in 1..<land.count {
+        for j in 0..<land[0].count {
+            for k in 0..<land[0].count {
+                if j == k { continue }
+                dp[i][j] = max(dp[i][j], dp[i-1][k] + land[i][j])
             }
         }
     }
     
-    return answer * answer
+    return dp.last!.max()!
 }
