db04c9ef8c7f8de1592b1531238d0cfa642eed04
InhyeKang <kanginhae0826@gmail.com>
2023-08-14 22:09:17 +0900

    튜플(64065)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -729,58 +729,82 @@ import Foundation
 
 
 // 수식 최대화(67257)
-enum Operation: Character {
-    case multiple = "*"
-    case plus = "+"
-    case minus = "-"
-    
-    func operate(_ n1: Int64, _ n2: Int64) -> Int64 {
-        switch self {
-        case .multiple:
-            return n1 * n2
-        case .plus:
-            return n1 + n2
-        case .minus:
-            return n1 - n2
-        }
-    }
-}
+//enum Operation: Character {
+//    case multiple = "*"
+//    case plus = "+"
+//    case minus = "-"
+//
+//    func operate(_ n1: Int64, _ n2: Int64) -> Int64 {
+//        switch self {
+//        case .multiple:
+//            return n1 * n2
+//        case .plus:
+//            return n1 + n2
+//        case .minus:
+//            return n1 - n2
+//        }
+//    }
+//}
+//
+//func operate(numbers: [Int64], opers: [Character], prior: [Operation]) -> Int64 {
+//    var numbers = numbers
+//    var opers = opers
+//
+//    for oper in prior {
+//        while let index = opers.firstIndex(of: oper.rawValue) {
+//            numbers[index] = oper.operate(numbers[index], numbers[index+1])
+//            numbers.remove(at: index+1)
+//            opers.remove(at: index)
+//        }
+//    }
+//
+//    return abs(numbers[0])
+//}
+//
+//func solution(_ expression:String) -> Int64 {
+//    let numbers = expression.components(separatedBy: ["*", "+", "-"]).map{ abs(Int64($0)!) }
+//    let opers = Array(expression.filter({ Int(String($0)) == nil }))
+//    let myOper = "*+-"
+//    var maxValue: Int64 = 0
+//
+//    for first in myOper {
+//        for second in myOper {
+//            guard first != second else { continue }
+//            let third = "*+-".filter({$0 != first && $0 != second})[0]
+//            let result = operate(
+//                numbers: numbers, opers: opers, prior: [
+//                    Operation(rawValue: first)!,
+//                    Operation(rawValue: second)!,
+//                    Operation(rawValue: third)!
+//                ]
+//            )
+//            maxValue = max(maxValue, result)
+//        }
+//    }
+//
+//    return maxValue
+//}
 
-func operate(numbers: [Int64], opers: [Character], prior: [Operation]) -> Int64 {
-    var numbers = numbers
-    var opers = opers
-    
-    for oper in prior {
-        while let index = opers.firstIndex(of: oper.rawValue) {
-            numbers[index] = oper.operate(numbers[index], numbers[index+1])
-            numbers.remove(at: index+1)
-            opers.remove(at: index)
-        }
-    }
-    
-    return abs(numbers[0])
-}
 
-func solution(_ expression:String) -> Int64 {
-    let numbers = expression.components(separatedBy: ["*", "+", "-"]).map{ abs(Int64($0)!) }
-    let opers = Array(expression.filter({ Int(String($0)) == nil }))
-    let myOper = "*+-"
-    var maxValue: Int64 = 0
+
+// 튜플(64065)
+func solution(_ s: String) -> [Int] {
+    var s = s
+    var answer = [Int]()
     
-    for first in myOper {
-        for second in myOper {
-            guard first != second else { continue }
-            let third = "*+-".filter({$0 != first && $0 != second})[0]
-            let result = operate(
-                numbers: numbers, opers: opers, prior: [
-                    Operation(rawValue: first)!,
-                    Operation(rawValue: second)!,
-                    Operation(rawValue: third)!
-                ]
-            )
-            maxValue = max(maxValue, result)
-        }
+    s.removeFirst(2)
+    s.removeLast(2)
+
+    s.components(separatedBy: "},{")
+        .map { $0.components(separatedBy: ",").map { Int($0)! } }
+        .sorted { $0.count < $1.count }
+        .forEach {
+            $0.forEach {
+                if !answer.contains($0) {
+                    answer.append($0)
+                }
+            }
     }
     
-    return maxValue
+    return answer
 }
