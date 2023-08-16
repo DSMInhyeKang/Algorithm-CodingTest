b701e4325c28515416b6043f241f3ef833e06dc2
InhyeKang <kanginhae0826@gmail.com>
2023-08-16 23:19:32 +0900

    오픈채팅방(42888)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -819,30 +819,32 @@ import Foundation
 
 
 // 오픈채팅방(42888)
+enum Commend: String {
+    case Enter  = "Enter"
+    case Leave  = "Leave"
+    case Change = "Change"
+}
+
 func solution(_ record: [String]) -> [String] {
-    let actions = ["Enter":"님이 들어왔습니다.", "Leave":"님이 나갔습니다."]
-    var a = [String:String]()
+    var user: [String: String] = [:]
+    var result: [(message: String, uid: String)] = []
     
-    record.forEach {
-        let separated = $0.components(separatedBy: " ")
+    for item in record {
+        let rec: [String] = item.split(separator: " ").map(String.init)
         
-        if separated.count > 2 {
-            a[separated[1]] = separated[2]
+        if let commend: Commend = Commend(rawValue: rec[0]) {
+            switch commend {
+            case .Enter:
+                user[rec[1]] = rec[2]
+                result.append(("님이 들어왔습니다.", rec[1]))
+            case .Leave:
+                result.append(("님이 나갔습니다.", rec[1]))
+            case .Change: user[rec[1]] = rec[2]
+            }
         }
     }
-    print(
-        record.filter { !$0.contains("Change") }.map { (value: String) -> String in
-            let separated = value.components(separatedBy: " ")
-            let newString = a[separated[1]]! + actions[separated[0]]!
-            
-            return newString
-        }
-    )
     
-    return record.filter { !$0.contains("Change") }.map { (value: String) -> String in
-        let separated = value.components(separatedBy: " ")
-        let newString = a[separated[1]]! + actions[separated[0]]!
-        
-        return newString
-    }
+    let answer: [String] = result.compactMap { "\(user[$0.uid]!)\($0.message)" }
+    
+    return answer
 }
