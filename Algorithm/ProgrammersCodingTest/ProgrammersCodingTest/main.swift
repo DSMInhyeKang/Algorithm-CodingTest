2ad5830e8db776984613a2fda8db888d57e48e23
InhyeKang <kanginhae0826@gmail.com>
2023-08-17 22:25:33 +0900

    이진 변환 반복하기(70129)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -819,32 +819,50 @@ import Foundation
 
 
 // 오픈채팅방(42888)
-enum Commend: String {
-    case Enter  = "Enter"
-    case Leave  = "Leave"
-    case Change = "Change"
-}
+//enum Commend: String {
+//    case Enter  = "Enter"
+//    case Leave  = "Leave"
+//    case Change = "Change"
+//}
+//
+//func solution(_ record: [String]) -> [String] {
+//    var user: [String: String] = [:]
+//    var result: [(message: String, uid: String)] = []
+//
+//    for item in record {
+//        let rec: [String] = item.split(separator: " ").map(String.init)
+//
+//        if let commend: Commend = Commend(rawValue: rec[0]) {
+//            switch commend {
+//            case .Enter:
+//                user[rec[1]] = rec[2]
+//                result.append(("님이 들어왔습니다.", rec[1]))
+//            case .Leave:
+//                result.append(("님이 나갔습니다.", rec[1]))
+//            case .Change: user[rec[1]] = rec[2]
+//            }
+//        }
+//    }
+//
+//    let answer: [String] = result.compactMap { "\(user[$0.uid]!)\($0.message)" }
+//
+//    return answer
+//}
 
-func solution(_ record: [String]) -> [String] {
-    var user: [String: String] = [:]
-    var result: [(message: String, uid: String)] = []
-    
-    for item in record {
-        let rec: [String] = item.split(separator: " ").map(String.init)
-        
-        if let commend: Commend = Commend(rawValue: rec[0]) {
-            switch commend {
-            case .Enter:
-                user[rec[1]] = rec[2]
-                result.append(("님이 들어왔습니다.", rec[1]))
-            case .Leave:
-                result.append(("님이 나갔습니다.", rec[1]))
-            case .Change: user[rec[1]] = rec[2]
-            }
-        }
+
+
+// 이진 변환 반복하기(70129)
+func solution(_ s: String) -> [Int] {
+    var s = s
+    var count = 0, times = 0
+
+    while s != "1" {
+        let replaceCount = s.filter { $0 == "0" }.count
+        count += replaceCount
+
+        s = String(s.count - replaceCount, radix: 2)
+        times += 1
     }
-    
-    let answer: [String] = result.compactMap { "\(user[$0.uid]!)\($0.message)" }
-    
-    return answer
+
+    return [times, count]
 }
