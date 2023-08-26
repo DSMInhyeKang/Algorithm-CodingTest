df4e5f5cd54a1ab9f74ecfabb9ae953a9a96c389
InhyeKang <kanginhae0826@gmail.com>
2023-08-26 23:52:39 +0900

    뒤에 있는 큰 수 찾기(154539)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -1035,20 +1035,40 @@ import Foundation
 
 
 // 바탕화면 정리(161990)
-func solution(_ wallpaper: [String]) -> [Int] {
-    var x_arr = [Int]()
-    var y_arr = [Int]()
+//func solution(_ wallpaper: [String]) -> [Int] {
+//    var x_arr = [Int]()
+//    var y_arr = [Int]()
+//
+//    for (i, paper) in wallpaper.enumerated() {
+//        if paper.contains("#"){
+//            for (index, w) in paper.map({String($0)}).enumerated(){
+//                if w == "#" {
+//                    x_arr.append(i)
+//                    y_arr.append(index)
+//                }
+//            }
+//        }
+//    }
+//
+//    return [x_arr.min() ?? 0, y_arr.min() ?? 0, x_arr.max()!+1, y_arr.max()!+1]
+//}
 
-    for (i, paper) in wallpaper.enumerated() {
-        if paper.contains("#"){
-            for (index, w) in paper.map({String($0)}).enumerated(){
-                if w == "#" {
-                    x_arr.append(i)
-                    y_arr.append(index)
-                }
+
+
+// 뒤에 있는 큰 수 찾기(154539)
+func solution(_ numbers: [Int]) -> [Int] {
+    var result: [Int] = Array(repeating: -1, count: numbers.count)
+    var stack: [(Int, Int)] = []
+    
+    for (i, n) in numbers.enumerated() {
+        if !stack.isEmpty {
+            while !stack.isEmpty && stack.last!.1 < n {
+                result[stack.removeLast().0] = n
             }
         }
+        
+        stack.append((i, n))
     }
-
-    return [x_arr.min() ?? 0, y_arr.min() ?? 0, x_arr.max()!+1, y_arr.max()!+1]
+    
+    return result
 }
