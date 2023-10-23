a0974461c12f657be6fef0439fe9da099f495c64
InhyeKang <kanginhae0826@gmail.com>
2023-10-23 19:17:52 +0900

    모의고사(42840)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -1399,26 +1399,46 @@ import Foundation
 
 
 // 문자열을 정수로 바꾸기(12925)
-func solution(_ s: String) -> Int {
-    return Int(s)!
-}
+//func solution(_ s: String) -> Int {
+//    return Int(s)!
+//}
 
 
 
 // 소수 찾기(12921)
-func solution(_ n:Int) -> Int {
-    var check = Array(repeating: 0, count: n + 1)
-    var cnt = 0
+//func solution(_ n:Int) -> Int {
+//    var check = Array(repeating: 0, count: n + 1)
+//    var cnt = 0
+//
+//    for i in 2...n {
+//        if check[i] == 0 {
+//            cnt += 1
+//            
+//            for j in stride(from: i, to: n + 1, by: i) {
+//                check[j] = 1
+//            }
+//        }
+//    }
+//
+//    return cnt
+//}
 
-    for i in 2...n {
-        if check[i] == 0 {
-            cnt += 1
-            
-            for j in stride(from: i, to: n + 1, by: i) {
-                check[j] = 1
-            }
-        }
+
+
+// 모의고사(42840)
+func solution(_ answers:[Int]) -> [Int] {
+    let answer = (
+        a: [1, 2, 3, 4, 5],
+        b: [2, 1, 2, 3, 2, 4, 2, 5],
+        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    )
+    var point = [1:0, 2:0, 3:0]
+
+    for (i, v) in answers.enumerated() {
+        if v == answer.a[i % 5] { point[1] = point[1]! + 1 }
+        if v == answer.b[i % 8] { point[2] = point[2]! + 1 }
+        if v == answer.c[i % 10] { point[3] = point[3]! + 1 }
     }
 
-    return cnt
+    return point.sorted{ $0.key < $1.key }.filter{ $0.value == point.values.max() }.map{ $0.key }
 }
