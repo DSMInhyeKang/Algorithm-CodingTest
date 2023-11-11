4f2104b0c8ccff4d278c81354c164790ac043f7c
InhyeKang <kanginhae0826@gmail.com>
2023-11-10 23:09:57 -0800

    요격 시스템(181188)

--- Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
+++ Algorithm/ProgrammersCodingTest/ProgrammersCodingTest/main.swift
@@ -1426,19 +1426,37 @@ import Foundation
 
 
 // 모의고사(42840)
-func solution(_ answers:[Int]) -> [Int] {
-    let answer = (
-        a: [1, 2, 3, 4, 5],
-        b: [2, 1, 2, 3, 2, 4, 2, 5],
-        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    )
-    var point = [1:0, 2:0, 3:0]
+//func solution(_ answers:[Int]) -> [Int] {
+//    let answer = (
+//        a: [1, 2, 3, 4, 5],
+//        b: [2, 1, 2, 3, 2, 4, 2, 5],
+//        c: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+//    )
+//    var point = [1:0, 2:0, 3:0]
+//
+//    for (i, v) in answers.enumerated() {
+//        if v == answer.a[i % 5] { point[1] = point[1]! + 1 }
+//        if v == answer.b[i % 8] { point[2] = point[2]! + 1 }
+//        if v == answer.c[i % 10] { point[3] = point[3]! + 1 }
+//    }
+//
+//    return point.sorted{ $0.key < $1.key }.filter{ $0.value == point.values.max() }.map{ $0.key }
+//}
 
-    for (i, v) in answers.enumerated() {
-        if v == answer.a[i % 5] { point[1] = point[1]! + 1 }
-        if v == answer.b[i % 8] { point[2] = point[2]! + 1 }
-        if v == answer.c[i % 10] { point[3] = point[3]! + 1 }
+
+
+// 요격 시스템(181188)
+func solution(_ targets: [[Int]]) -> Int {
+    var answer = 0
+    let sorted = targets.sorted(by: { $0[1] < $1[1] })
+    
+    var end = sorted[0][1]
+
+    for target in sorted {
+        if target[0] >= end {
+            end = target[1]
+            answer += 1
+        }
     }
-
-    return point.sorted{ $0.key < $1.key }.filter{ $0.value == point.values.max() }.map{ $0.key }
+    return answer + 1
 }
