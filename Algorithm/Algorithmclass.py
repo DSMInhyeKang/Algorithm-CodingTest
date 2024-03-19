ec94747fc8e00d5a2a73e80d3e5144fe63ffb742
InhyeKang <kanginhae0826@gmail.com>
2024-03-19 15:55:34 +0900

    부분집합 구하기(DFS), 합이 같은 부분집합(DFS : 아마존 인터뷰), 바둑이 승차(DFS), 중복순열 구하기

--- Algorithm/Algorithmclass.py
+++ Algorithm/Algorithmclass.py
@@ -29,7 +29,7 @@ print(len([n for n in range(N+1) if primes[n]]))
 
 
 
-# 요세푸스 문제
+# 요세푸스 문제 (알실)
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
@@ -83,3 +83,117 @@ while len(princes) > 1:
         del princes[0]
 
 print(princes)
+
+
+
+# 괄호 (알실)
+T = int(input())
+
+for i in range(T):
+    val = input()
+    stack = []
+
+    for c in list(val):
+        if c == '(':
+            stack.append(c)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                print("NO")
+                break
+
+    if stack: print("NO")
+    else: print("YES")
+
+
+
+# 부분집합 구하기(DFS)
+N = int(input())
+visited= [False for _ in range(N+1)]
+
+def dfs(n):
+    if n == N+1:
+        for i in range(1, N+1):
+            if visited[i] == True:
+                print(i, end=" ")
+        print()
+        return   
+    else:
+        visited[n] = True
+        dfs(n+1)
+        visited[n] = False
+        dfs(n+1)
+
+dfs(1)
+
+
+
+# 합이 같은 부분집합(DFS : 아마존 인터뷰)
+import sys
+
+N = int(input())
+num = list(map(int, input().split()))
+totalSum = sum(num)
+
+def dfs(v, sum):
+    if sum > totalSum//2:  # 문제에서 요구하는 조건과 부합하지 않기 때문에 탐색할 필요 없음
+        return
+    
+    if v == N:
+        if sum == totalSum//2:
+            print("YES")
+            sys.exit(0)
+    else:
+        dfs(v+1, sum+num[v])
+        dfs(v+1, sum)
+
+dfs(0, 0)
+print("NO")
+
+
+
+# 바둑이 승차(DFS)
+C, N = map(int, input().split())
+weight = [int(input()) for _ in range(N)]
+max = 0
+
+def dfs(v, sum):
+    global max
+
+    if sum > C:
+        return
+
+    if v == N:
+        if sum <= C and sum > max:
+            max = sum
+            return
+    else:
+        dfs(v+1, sum+weight[v])
+        dfs(v+1, sum)
+
+dfs(0, 0)
+print(max)
+
+
+
+#  중복순열 구하기
+N, M = map(int, input().split())
+cnt = 0
+h = [0] * M
+
+def dfs(v):
+    global cnt
+    
+    if v == M:
+        for i in range(M):
+            print(h[i], end=" ")
+        print()
+        cnt += 1
+    else:
+        for j in range(1, N+1):
+            h[v] = j
+            dfs(v+1)
+
+dfs(0)
+print(cnt)
