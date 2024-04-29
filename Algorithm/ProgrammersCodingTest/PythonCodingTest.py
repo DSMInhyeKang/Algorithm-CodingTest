573e8d6f9cec842af6d9525219ee63c2b30a24a9
InhyeKang <kanginhae0826@gmail.com>
2024-04-29 23:40:43 +0900

    멀리 뛰기(12914) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -497,3 +497,17 @@ def solution(land):
                     reclamed[l] += oil
 
     return max(reclamed)
+
+
+
+# 멀리 뛰기(12914) - Lv.2
+def solution(n):
+    dp = [0] * (n+1)
+    
+    dp[0] = 1
+    dp[1] = 2
+    
+    for i in range(2, n):
+        dp[i] = dp[i-2] + dp[i-1]
+	
+    return dp[n-1] % 1234567
