8fb472063d42cb3ac672fa7f50b4856417a108ca
InhyeKang <kanginhae0826@gmail.com>
2024-05-12 23:12:34 +0900

    BOJ - N-Queen(9633)

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -679,3 +679,26 @@ def solution(bandage, health, attacks):
         time = sec
 
     return hp
+
+
+
+# BOJ - N-Queen(9633)
+n, ans = int(input()), 0
+a, b, c = [False] * n, [False] * (2*n-1), [False] * (2*n-1)
+
+def backtracking(i):
+    global ans
+    
+    if i == n:
+        ans += 1
+        return
+    
+    for j in range(n):
+        if not (a[j] or b[i+j] or c[i-j+n-1]):
+            a[j] = b[i+j] = c[i-j+n-1] = True
+            backtracking(i+1)
+            a[j] = b[i+j] = c[i-j+n-1] = False
+
+
+backtracking(0)
+print(ans)
