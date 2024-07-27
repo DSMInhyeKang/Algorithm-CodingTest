d58498a90c6ae9d846e91db5a190f21154739ad9
InhyeKang <kanginhae0826@gmail.com>
2024-07-27 14:20:56 +0900

    에어컨(214289) - Lv.3

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -901,3 +901,39 @@ def solution(topping):
         if len(dic) == len(set_dic): res += 1
             
     return res
+
+
+
+# 에어컨(214289) - Lv.3
+def solution(temperature, t1, t2, a, b, onboard):
+    k = 1000 * 100
+    t1 += 10
+    t2 += 10
+    temperature += 10
+    
+    dp = [[k] * 51 for _ in range(len(onboard))]
+    dp[0][temperature] = 0
+    
+    flag = 1
+    if temperature > t2:
+        flag = -1
+        
+    for i in range(1, len(onboard)):
+        for j in range(51):
+            arr = [k]
+            
+            if (onboard[i] == 1 and t1 <= j <= t2) or onboard[i] == 0:
+                if 0 <= j+flag <= 50:
+                    arr.append(dp[i-1][j+flag])
+                if j == temperature:
+                    arr.append(dp[i-1][j])
+                if 0 <= j-flag <= 50:
+                    arr.append(dp[i-1][j-flag] + a)
+                if t1 <= j <= t2:
+                    arr.append(dp[i-1][j] + b)
+
+                dp[i][j] = min(arr)
+            
+    answer = min(dp[len(onboard)-1])
+    
+    return answer
