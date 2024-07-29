30342295ac133d248ac1c3f52e7089ce1cfc5893
InhyeKang <kanginhae0826@gmail.com>
2024-07-29 11:57:31 +0900

    주차 요금 계산(92341) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -1037,3 +1037,71 @@ def bfs(rx, ry, bx, by, rex, rey, bex, bey, maze, redVisited, blueVisited):
                 _blueVisited.pop()
 
     return 0
+
+
+
+# 주차 요금 계산(92341) - Lv.2
+from datetime import datetime
+import math
+
+def solution(fees, records):
+    answer = { record.split()[1]:0 for record in records }
+    car = {}
+    
+    for record in records:
+        time, car_num, inout = record.split()
+
+        if inout == 'IN':
+            car[car_num] = datetime.strptime(time,"%H:%M")
+        else:
+            pay = str(datetime.strptime(time,"%H:%M") - car[car_num])
+            pay = int(pay.split(':')[0]) * 60 + int(pay.split(':')[1])
+            answer[car_num] += pay
+            del car[car_num]
+
+    if len(car) > 0:
+        for i in car:
+            pay = str(datetime.strptime('23:59',"%H:%M") - car[i])
+            pay = int(pay.split(':')[0]) * 60 + int(pay.split(':')[1])
+            answer[i] += pay
+
+    answer = sorted(answer.items())
+    
+    for i in range(len(answer)):
+        if answer[i][1] <= fees[0]:
+            answer[i] = fees[1]
+        else:
+            answer[i] = fees[1] + math.ceil((answer[i][1] - fees[0])/fees[2]) * fees[3]
+
+    return answer
+
+# Class + Lambda 활용 풀이
+from collections import defaultdict
+from math import ceil
+
+class Parking:
+    def __init__(self, fees):
+        self.fees = fees
+        self.in_flag = False
+        self.in_time = 0
+        self.total = 0
+
+    def update(self, t, inout):
+        self.in_flag = True if inout=='IN' else False
+        if self.in_flag:  self.in_time = str2int(t)
+        else:             self.total  += (str2int(t)-self.in_time)
+
+    def calc_fee(self):
+        if self.in_flag: self.update('23:59', 'out')
+        add_t = self.total - self.fees[0]
+        return self.fees[1] + ceil(add_t/self.fees[2]) * self.fees[3] if add_t >= 0 else self.fees[1]
+
+def str2int(string):
+    return int(string[:2])*60 + int(string[3:])
+
+def solution(fees, records):
+    recordsDict = defaultdict(lambda:Parking(fees))
+    for rcd in records:
+        t, car, inout = rcd.split()
+        recordsDict[car].update(t, inout)
+    return [v.calc_fee() for k, v in sorted(recordsDict.items())]
