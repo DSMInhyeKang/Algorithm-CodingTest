f1fbbd15816eb7f07ec18b9c47025898c937f0d1
InhyeKang <kanginhae0826@gmail.com>
2024-07-30 08:53:16 +0900

    디펜스 게임(142085) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -1105,3 +1105,20 @@ def solution(fees, records):
         t, car, inout = rcd.split()
         recordsDict[car].update(t, inout)
     return [v.calc_fee() for k, v in sorted(recordsDict.items())]
+
+
+
+# 디펜스 게임(142085) - Lv.2
+import heapq as hq
+
+def solution(n, k, enemy):
+    q = enemy[:k]
+    hq.heapify(q)
+
+    for idx in range(k,len(enemy)):
+        n -= hq.heappushpop(q, enemy[idx])
+
+        if n < 0:
+            return idx
+        
+    return len(enemy)
