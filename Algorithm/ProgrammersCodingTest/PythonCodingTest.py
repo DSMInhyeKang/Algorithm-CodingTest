815f7ad0dbd79c58a42ed33500d46f9d6b97ddb1
InhyeKang <kanginhae0826@gmail.com>
2024-08-27 23:38:27 +0900

    BOJ - 톱니바퀴(14891): G5

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -1668,3 +1668,43 @@ for x in range(N):
             answer += 10 ** (friend - 1)
 
 print(answer)
+
+
+
+# BOJ - 톱니바퀴(14891): G5
+from collections import deque
+
+def right(idx, d):
+    if idx > 3:
+        return
+
+    if sawtooth[idx - 1][2] != sawtooth[idx][6]:
+        right(idx + 1, -d)
+        sawtooth[idx].rotate(d)
+
+def left(idx, d):
+    if idx < 0:
+        return
+    
+    if sawtooth[idx][2] != sawtooth[idx + 1][6]:
+        left(idx - 1, -d)
+        sawtooth[idx].rotate(d)
+
+sawtooth = [deque(list(map(int, input()))) for _ in range(4)]
+k = int(input())
+
+for _ in range(k):
+    idx, d = map(int, input().split())
+    idx -= 1
+    left(idx-1, -d)
+    right(idx+1, -d)
+
+    sawtooth[idx].rotate(d)
+
+score = 0
+
+for i in range(4):
+    if sawtooth[i][0] == 1:
+        score += 2 ** i
+
+print(score)
