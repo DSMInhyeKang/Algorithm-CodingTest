22fcc983e91d0dc250389e4fc04507a6705f43d9
InhyeKang <kanginhae0826@gmail.com>
2024-09-03 22:13:50 +0900

    두 큐 합 같게 만들기(118667) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -1947,3 +1947,31 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
                     q.append((nx,ny))
                     
     return answer
+
+
+
+# 두 큐 합 같게 만들기(118667) - Lv.2
+from collections import deque
+ 
+def solution(queue1, queue2):
+    answer = 0
+    q1, q2 = deque(queue1), deque(queue2)
+    sum1, sum2 = sum(q1), sum(q2)
+    limit = len(q1) * 3
+    
+    while sum1 != sum2:
+        if sum1 > sum2:
+            sum1 -= q1[0]
+            sum2 += q1[0]
+            q2.append(q1.popleft())
+        elif sum1 < sum2:
+            sum1 += q2[0]
+            sum2 -= q2[0]
+            q1.append(q2.popleft())
+            
+        answer += 1
+        
+        if answer == limit: 
+            return -1
+        
+    return answer
