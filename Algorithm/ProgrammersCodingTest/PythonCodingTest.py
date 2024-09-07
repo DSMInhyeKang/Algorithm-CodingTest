78b59370438a042202dfa830c9bcb0d9f5a816a6
InhyeKang <kanginhae0826@gmail.com>
2024-09-07 23:32:59 +0900

    [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지(340212) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -2059,3 +2059,35 @@ def solution(places):
         answer.append(check_distance(place))
 
     return answer
+
+
+
+# [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지(340212) - Lv.2
+def solution(diffs, times, limit):
+    left, right = 1, max(diffs)
+    answer = right
+
+    def solveTime(level):
+        total = 0
+        prev = times[0]
+
+        for i in range(len(diffs)):
+            diff = diffs[i]
+            cur = times[i]
+
+            if diff <= level: total += cur
+            else:
+                mistakes = diff - level
+                total += (mistakes * (cur + prev)) + cur
+            prev = cur
+
+        return total
+
+    while left <= right:
+        mid = (left + right) // 2
+        if solveTime(mid) <= limit:
+            answer = mid
+            right = mid - 1
+        else: left = mid + 1
+
+    return answer
