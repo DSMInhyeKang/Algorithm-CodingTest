895c4abf7d339cb09b51ffa9ead537097ee5179b
InhyeKang <kanginhae0826@gmail.com>
2024-09-09 22:25:17 +0900

    마법의 엘리베이터(148653) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -2114,3 +2114,14 @@ def solution(n):
             num += 1     
             
     return sum(answer, [])
+
+
+
+# 마법의 엘리베이터(148653) - Lv.2
+def solution(storey):
+    if storey < 10 :
+        return min(storey, 11 - storey)
+    
+    left = storey % 10
+    
+    return min(left + solution(storey // 10), 10 - left + solution(storey // 10 + 1))
