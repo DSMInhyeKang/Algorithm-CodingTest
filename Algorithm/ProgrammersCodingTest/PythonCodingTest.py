4478dce5a9cfaeb7d5a74d89b5b4fe9b0d3db064
InhyeKang <kanginhae0826@gmail.com>
2024-09-11 23:22:54 +0900

    보석 쇼핑(67258) - Lv.3

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -2146,3 +2146,35 @@ def solution(enroll, referral, seller, amount):
             s = referral[idx]
             
     return money
+
+
+
+# 보석 쇼핑(67258) - Lv.3
+def solution(gems):
+    N = len(gems)
+    answer = [0, N-1]
+    kind = len(set(gems))
+    dic = { gems[0]: 1, }
+    s, e = 0, 0 
+    
+    while s < N and e < N:
+        if len(dic) < kind:  
+            e += 1
+            
+            if e == N: break
+                
+            dic[gems[e]] = dic.get(gems[e], 0) + 1
+        else: 
+            if (e - s + 1) < (answer[1] - answer[0] + 1): answer = [s, e]
+                
+            if dic[gems[s]] == 1:
+                del dic[gems[s]]
+            else:
+                dic[gems[s]] -= 1
+                
+            s += 1
+
+    answer[0] += 1
+    answer[1] += 1
+    
+    return answer
