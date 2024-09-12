7bc8d84edeefae92d784269e15bb9881a1203669
InhyeKang <kanginhae0826@gmail.com>
2024-09-12 22:53:51 +0900

    괄호 회전하기(76502) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -2178,3 +2178,29 @@ def solution(gems):
     answer[1] += 1
     
     return answer
+
+
+
+# 괄호 회전하기(76502) - Lv.2
+def solution(s):
+    answer = 0
+    s = list(s)
+    
+    for _ in range(len(s)):
+        stack = []
+        
+        for i in range(len(s)):
+            if len(stack) > 0:
+                if stack[-1] == '[' and s[i] == ']': stack.pop()
+                elif stack[-1] == '{' and s[i] == '}': stack.pop()
+                elif stack[-1] == '(' and s[i] == ')': stack.pop()
+                else: stack.append(s[i])
+            else:
+                stack.append(s[i])
+                
+        if len(stack) == 0:
+            answer += 1
+            
+        s.append(s.pop(0))
+
+    return answer
