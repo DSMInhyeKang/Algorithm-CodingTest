445ebf1e9527be4407527d075b7380b3eeaab2c0
InhyeKang <kanginhae0826@gmail.com>
2024-09-24 10:35:23 +0900

    행렬 테두리 회전하기(77485) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -2339,3 +2339,45 @@ def solution(n, weak, dist):
                     q.append(list(temp))
                     
     return -1
+
+
+
+# 행렬 테두리 회전하기(77485) - Lv.2
+def solution(rows, columns, queries):
+    arr = [[0 for c in range(columns)] for r in range(rows)]
+    answer = []
+    t = 1
+    
+    for row in range(rows):
+        for col in range(columns):
+            arr[row][col] = t
+            t += 1
+
+    for x1, y1, x2, y2 in queries:
+        tmp = arr[x1-1][y1-1]
+        mini = tmp
+
+        for k in range(x1-1,x2-1):
+            test = arr[k+1][y1-1]
+            arr[k][y1-1] = test
+            mini = min(mini, test)
+
+        for k in range(y1-1,y2-1):
+            test = arr[x2-1][k+1]
+            arr[x2-1][k] = test
+            mini = min(mini, test)
+
+        for k in range(x2-1,x1-1,-1):
+            test = arr[k-1][y2-1]
+            arr[k][y2-1] = test
+            mini = min(mini, test)
+
+        for k in range(y2-1,y1-1,-1):
+            test = arr[x1-1][k-1]
+            arr[x1-1][k] = test
+            mini = min(mini, test)
+
+        arr[x1-1][y1] = tmp
+        answer.append(mini)
+        
+    return answer
