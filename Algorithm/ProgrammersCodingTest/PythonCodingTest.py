468f4a2f1773bbbbaf26cc667da49e9ea0cc2bce
InhyeKang <kanginhae0826@gmail.com>
2024-10-07 23:09:31 +0900

    메뉴 리뉴얼(72411) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -2569,3 +2569,25 @@ def solution(begin, end):
     answer = [get_div(i) for i in range(begin, end+1)]
     
     return answer
+
+
+
+# 메뉴 리뉴얼(72411) - Lv.2
+from itertools import combinations
+from collections import Counter
+
+def solution(orders, course):
+    answer = []
+    
+    for k in course:
+        candidates = []
+        
+        for menu_li in orders:
+            for li in combinations(menu_li, k):
+                res = ''.join(sorted(li))
+                candidates.append(res)
+                
+        sorted_candidates = Counter(candidates).most_common()
+        answer += [menu for menu, cnt in sorted_candidates if cnt > 1 and cnt == sorted_candidates[0][1]]
+        
+    return sorted(answer)
