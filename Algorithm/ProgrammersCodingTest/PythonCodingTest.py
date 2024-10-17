fb6e2e29f205f533de441c18497553b7eaecc58f
InhyeKang <kanginhae0826@gmail.com>
2024-10-17 13:52:03 +0900

    기둥과 보 설치(60061) - Lv.3

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -2869,3 +2869,34 @@ def solution(n, s, a, b, fares):
         if d[i][j] > (l := d[i][k] + d[k][j]): d[i][j] = l
 
     return min([d[s][k]+d[k][a]+d[k][b] for k in range(n)])
+
+
+
+# 기둥과 보 설치(60061) - Lv.3
+def check(answer):
+    for x, y, stuff in answer:
+        if stuff == 0:
+            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer: continue
+            return False
+        elif stuff == 1: 
+            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer): continue
+            return False
+        
+    return True
+
+def solution(n, build_frame):
+    answer = []
+    
+    for build in build_frame:
+        x, y, stuff, operation = build
+        
+        if operation == 1:
+            answer.append([x, y, stuff])
+            if not check(answer): answer.remove([x, y, stuff])
+        elif operation == 0:
+            answer.remove([x, y, stuff])
+            if not check(answer): answer.append([x, y, stuff])
+            
+    answer.sort()
+    
+    return answer
