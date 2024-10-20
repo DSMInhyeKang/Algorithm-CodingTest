1e310fcd880e598bad9daff219633e679e168f95
InhyeKang <kanginhae0826@gmail.com>
2024-10-20 22:16:05 +0900

    [1차] 뉴스 클러스터링(17677) - Lv.3

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -2930,3 +2930,23 @@ def order(nodes, levels, curlevel):
                     break
 
     post.append(cur[0])
+
+
+
+# [1차] 뉴스 클러스터링(17677) - Lv.3
+import re
+import math
+
+def solution(str1, str2):
+    str1 = [str1[i:i+2].lower() for i in range(0, len(str1)-1) if not re.findall('[^a-zA-Z]+', str1[i:i+2])]
+    str2 = [str2[i:i+2].lower() for i in range(0, len(str2)-1) if not re.findall('[^a-zA-Z]+', str2[i:i+2])]
+
+    gyo = set(str1) & set(str2)
+    hap = set(str1) | set(str2)
+
+    if len(hap) == 0: return 65536
+
+    gyo_sum = sum([min(str1.count(gg), str2.count(gg)) for gg in gyo])
+    hap_sum = sum([max(str1.count(hh), str2.count(hh)) for hh in hap])
+
+    return math.floor((gyo_sum/hap_sum)*65536)
