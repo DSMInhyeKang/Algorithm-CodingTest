824e7d77d212bce498cf1c5a80458d6b68050e67
InhyeKang <kanginhae0826@gmail.com>
2024-10-24 12:25:15 +0900

    경주로 건설(67259) - Lv.3

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -3055,3 +3055,33 @@ for c in combinations(point, 3):
     bfs(tmp)
 
 print(answer)
+
+
+
+# 경주로 건설(67259) - Lv.3
+from heapq import heappop, heappush
+from sys import maxsize
+
+def solution(board):
+    N = len(board)
+    costBoard = [[[maxsize] * N for _ in range(N)] for _ in range(4)]
+    heap = [(0, 0, 0, 0), (0, 0, 0, 2)]
+    
+    for i in range(4): 
+        costBoard[i][0][0] = 0
+    
+    while heap:
+        cost, x, y, d = heappop(heap)
+        
+        for dx, dy, dd in ((1, 0, 0), (-1, 0, 1), (0, 1, 2), (0, -1, 3)):
+            nx, ny = x + dx, y + dy
+            
+            if nx < 0 or nx >= N or ny < 0 or ny >= N or board[ny][nx]: continue
+            
+            newCost = cost + (100 if d == dd else 600)
+            
+            if costBoard[dd][ny][nx] > newCost:
+                costBoard[dd][ny][nx] = newCost
+                heappush(heap, (newCost,nx,ny,dd))
+                
+    return min(costBoard[0][N-1][N-1], costBoard[1][N-1][N-1], costBoard[2][N-1][N-1], costBoard[3][N-1][N-1])
