f4b6c6a018bcf80c55814b47733a37c376daa2e5
InhyeKang <kanginhae0826@gmail.com>
2024-11-30 23:54:26 +0900

    [1차] 프렌즈4블록(17679) - Lv.2

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -3628,3 +3628,32 @@ def solution(scoville, K):
             break
         
     return -1 if scoville[0] < K else cnt
+
+
+
+# [1차] 프렌즈4블록(17679) - Lv.2
+def solution(m, n, board):
+    board.reverse()
+    field = [list(cols) for cols in zip(*board)]
+    answer = 0
+
+    while True:
+        bomb = set()
+        
+        for row in range(n - 1):
+            for col in range(m - 1):
+                try:
+                    if field[row][col] == field[row + 1][col] == field[row][col + 1] == field[row + 1][col + 1]:
+                        bomb.update({(row, col), (row + 1, col), (row, col + 1), (row + 1, col + 1)})
+                except:
+                    break
+
+        if not len(bomb): break
+
+        for r, c in bomb:
+            field[r][c] = ''
+            answer += 1
+
+        for row in range(n): field[row] = list(''.join(field[row]))
+
+    return answer
