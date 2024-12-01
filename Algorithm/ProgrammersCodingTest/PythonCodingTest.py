c6b0cdf358b5956f145dc5fe76e84012c2edef81
InhyeKang <kanginhae0826@gmail.com>
2024-12-01 21:21:49 +0900

    등산코스 정하기(118669) - Lv.3

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -3657,3 +3657,36 @@ def solution(m, n, board):
         for row in range(n): field[row] = list(''.join(field[row]))
 
     return answer
+
+
+
+# 등산코스 정하기(118669) - Lv.3
+def solution(n, paths, gates, summits):
+    field = {}
+    intensity = [10000001] * (n+1)
+    check, stack = set(summits), gates
+
+    for A, B, time in paths:
+        for x, y in ((A, B), (B, A)):
+            try: field[x].append((y, time))
+            except: field[x] = [(y, time)]
+
+    for g in gates: intensity[g] = 0
+
+    while stack:
+        target = set()
+
+        while stack:
+            now = stack.pop()
+
+            for to, time in field[now]:
+                max_time = max(intensity[now], time)
+
+                if intensity[to] > max_time:
+                    intensity[to] = max_time
+
+                    if to not in check: target.add(to)
+
+        stack = list(target)
+
+    return min([[summit, intensity[summit]] for summit in summits], key=lambda x: (x[1], x[0]))
