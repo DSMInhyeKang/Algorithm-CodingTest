dab5ab0f8d7ab3a01b5e5a1d3a081b9d1f3ae42f
InhyeKang <kanginhae0826@gmail.com>
2024-12-03 00:20:29 +0900

    풍선 터트리기(68646) - Lv.3

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -3715,3 +3715,27 @@ def solution(alp, cop, problems):
                 if ca >= ra and cc >= rc and curr_cost + cost < t[na][nc]:
                     t[na][nc] = curr_cost + cost
                     heappush(h, (curr_cost + cost, na, nc))
+
+
+
+# 풍선 터트리기(68646) - Lv.3
+def solution(a):
+    result = [False] * len(a)
+    rear, front = float('inf'), float('inf')
+    
+    for i in range(len(a)):
+        if a[i] < rear:
+            rear = a[i]
+            result[i] = True
+        if a[-1-i] < front:
+            front = a[-1-i]
+            result[-1-i] = True
+            
+    return sum(result)
+
+
+def solution(a, m=1000000001, n=1000000001):
+    l = [(m := min(m, v)) for v in a[:-1]]
+    r = [(n := min(n, v)) for v in reversed(a[2:])]
+    
+    return sum((x > y or z > y for x, y, z in zip(l, a[1:-1], reversed(r)))) + 2
