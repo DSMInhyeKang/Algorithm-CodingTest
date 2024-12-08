adcc0ebd4940e2f8b7c7142b9e6b0bf82cfbef42
InhyeKang <kanginhae0826@gmail.com>
2024-12-08 14:11:15 +0900

    2차원 동전 뒤집기(131703) - Lv.3

--- Algorithm/ProgrammersCodingTest/PythonCodingTest.py
+++ Algorithm/ProgrammersCodingTest/PythonCodingTest.py
@@ -3837,3 +3837,21 @@ def solution(N, number):
             break
     
     return answer
+
+
+
+# 2차원 동전 뒤집기(131703) - Lv.3
+def solution(beginning, target):
+    answer, cnt = 0, 0
+    table = [[beginning[i][j] ^ target[i][j] for j in range(len(beginning[i]))] for i in range(len(beginning))]
+    m, n = len(table), len(table[0])
+
+    for i in range(1, m):
+        if table[i] != table[0]:
+            cnt += 1
+
+            if (list(map(lambda x: x^1, table[i])) != table[0]): return -1
+
+    answer = min(cnt+sum(table[0]), m-cnt+n-sum(table[0]))
+
+    return answer
